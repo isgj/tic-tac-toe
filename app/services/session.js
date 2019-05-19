6e13fc24017fa87303250d8ea88d797fa242d9e1
Ismail Gjevori <isgjevori@protonmail.com>
2019-05-19 18:36:01 +0200

    add login page

--- app/services/session.js
+++ app/services/session.js
@@ -1,4 +1,20 @@
 import Service from '@ember/service';
 
+const TOKEN_KEY = '_token';
+
 export default Service.extend({
+  init() {
+    this._super();
+    const token = localStorage.getItem(TOKEN_KEY);
+    if (token && token.length) this.set('token', token)
+  },
+  token: null,
+  login(token) {
+    this.set('token', token);
+    localStorage.setItem(TOKEN_KEY, token)
+  },
+  logout() {
+    this.set('token', null);
+    localStorage.setItem(TOKEN_KEY, '');
+  }
 });
